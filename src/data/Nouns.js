0b2c6dada75d0cfedeeac41d6fa66d8348bb3381
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-26 23:25:43 +0530

    raw data for Nouns

---
+++ src/data/Nouns.js
@@ -0,0 +1,71 @@
+import { ethers } from "ethers";
+
+export const Nouns = [
+  {
+    nounId: 1,
+    image: "https://noun.pics/0",
+    price: "0.1",
+  },
+  {
+    nounId: 2,
+    image: "https://noun.pics/0",
+    price: "0.4",
+  },
+  {
+    nounId: 3,
+    image: "https://noun.pics/0",
+    price: "0.5",
+  },
+
+  {
+    nounId: 4,
+    image: "https://noun.pics/0",
+    price: "0.6",
+  },
+];
+export const tNouns = [
+  {
+    nounId: 1,
+    image: "Banana",
+    price: "0.1",
+  },
+  {
+    nounId: 2,
+    image: "Apple",
+    price: "0.4",
+  },
+  {
+    nounId: 3,
+    image: "Watermelon",
+    price: "0.5",
+  },
+
+  {
+    nounId: 4,
+    image: "Pineapple",
+    price: "0.6",
+  },
+];
+export const fNouns = [
+  {
+    nounId: 1,
+    image: "Banana",
+    price: "0.1",
+  },
+  {
+    nounId: 2,
+    image: "Apple",
+    price: "0.4",
+  },
+  {
+    nounId: 3,
+    image: "Watermelon",
+    price: "0.5",
+  },
+
+  {
+    nounId: 4,
+    image: "Pineapple",
+    price: "0.6",
+  },
+];
