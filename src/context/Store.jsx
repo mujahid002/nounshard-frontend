041d7b6b51d2d3c43ae3811119ec362acbe25378
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-26 23:29:04 +0530

    Global Context

---
+++ src/context/Store.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+
+// Create the context
+const GlobalContext = createContext();
+
+export const GlobalContextProvider = ({ children }) => {
+  const [userAddress, setUserAddress] = useState("");
+  const [nativeBalance, setNativeBalance] = useState("");
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        userAddress,
+        setUserAddress,
+        nativeBalance,
+        setNativeBalance,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  const context = useContext(GlobalContext);
+  if (context === undefined) {
+    throw new Error(
+      "useGlobalContext must be used within a GlobalContextProvider"
+    );
+  }
+  return context;
+};
