2c1a1e68551eeec370f6b005d37f7ab296e28bc2
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-27 17:25:03 +0530

    updated layout with Logo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,13 +5,13 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>NounShard</title>
+        <title>NounShards</title>
         <meta
           name="description"
           content="A simple website to Tokenize the Noun"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/nounshards.ico" />
       </Head>
       <NavBar />
       <main className="bg-[#f8f7f5] min-h-[calc(100vh-76px)] px-10 py-8">
