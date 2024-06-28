0f060a1d5750e667e7ded6ed72e6cc656b12ec8d
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-28 19:21:27 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Noun from "../components/Noun";
-import { Nouns } from "../data/Nouns";
+import FetchNoun from "../components/FetchNoun";
+import { Nouns } from "@/data/Nouns";
 
 export default function Home() {
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center mx-auto gap-4 place-center flex-wrap w-100 md:max-w-[900px]">
+    <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center mx-auto gap-4 place-center flex-wrap w-full md:max-w-[900px]">
       {Nouns.map((noun) => (
-        <Noun noun={noun} key={noun.nounId} />
+        <FetchNoun key={noun.nounId} noun={noun} />
       ))}
     </div>
   );
