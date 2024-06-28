27499cc18c0b4b66a8b8faaa93078a84955a3f7c
Mujahid002 <mujahidshaik2002@gmail.com>
2024-06-28 22:55:27 +0530

    working!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,26 @@
-import React from "react";
-// import {FetchNoun} from "../components/FetchNoun";
-import { Nouns } from "@/data/Nouns";
+"use client";
+import React, { useState } from "react";
+import { Nouns as initialNouns } from "@/constants/index";
 import { FetchNoun } from "@/components/FetchNoun";
 
-export default function Home() {
+const ParentComponent = () => {
+  const [nouns, setNouns] = useState(initialNouns);
+
+  const updateNoun = (updatedNoun) => {
+    setNouns((prevNouns) =>
+      prevNouns.map((noun) =>
+        noun.nounId === updatedNoun.nounId ? updatedNoun : noun
+      )
+    );
+  };
+
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-4 justify-center mx-auto gap-4 place-center flex-wrap w-full md:max-w-[900px]">
-      {Nouns.map((noun) => (
-        <FetchNoun key={noun.nounId} noun={noun} />
+    <div>
+      {nouns.map((noun) => (
+        <FetchNoun key={noun.nounId} noun={noun} updateNoun={updateNoun} />
       ))}
     </div>
   );
-}
+};
+
+export default ParentComponent;
